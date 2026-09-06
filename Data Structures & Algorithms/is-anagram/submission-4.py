953d080d8668cdd59b2ce8fdd5ef69a06e5d6e70
rino-genistus <rinogenistus@gmail.com>
2026-09-06 17:42:10 -0400

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        word1 = {}
+        word2 = {}
+        for char in s:
+            if char not in word1.keys():
+                word1[char] = 1
+            else:
+                word1[char] += 1
+        for char in t:
+            if char not in word2.keys():
+                word2[char] = 1
+            else:
+                word2[char] += 1
+        word1 = dict(sorted(word1.items()))
+        word2 = dict(sorted(word2.items()))
+        return word1 == word2
