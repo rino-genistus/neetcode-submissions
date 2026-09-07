8e44be6d26bb280b3393274ccd0971db7bf7816e
rino-genistus <rinogenistus@gmail.com>
2026-09-07 00:49:18 -0400

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        if len(strs) == 1:
+            return [strs]
+        anagram_dict = {}
+        for word in strs:
+            curr = "".join(sorted(word))
+            if curr in anagram_dict:
+                anagram_dict[curr].append(word)
+            else:
+                added_word = "".join(sorted(word))
+                anagram_dict[added_word] = []
+                anagram_dict[added_word].append(word)
+        return_list = []
+        for l in anagram_dict:
+            return_list.append(anagram_dict[l])
+        return return_list
